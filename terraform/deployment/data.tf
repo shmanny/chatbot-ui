5a047406fddff6a12b0094e6333cad2608ba5de0
shmanny <aman06@gmail.com>
2023-07-09 08:43:48 -0600

    updated config

---
+++ terraform/deployment/data.tf
@@ -0,0 +1,26 @@
+data "aws_caller_identity" "current" {
+}
+
+data "aws_vpc" "main" {
+  tags = {
+    Name = "${var.app_name}-vpc"
+  }
+}
+
+data "aws_subnets" "public" {
+  # vpc_id = data.aws_vpc.main.id
+  tags = {
+	Type = "public"
+  }
+}
+
+data "aws_subnets" "private" {
+  # vpc_id = data.aws_vpc.main.id
+  tags = {
+	Type = "private"
+  }
+}
+
+variable "app_name" {
+  type = string
+}
