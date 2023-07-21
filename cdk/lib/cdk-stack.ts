a4ec2eb982a5f2f52b576bf196e79a4fcad1acb2
Aman Saran <aman06@gmail.com>
2023-07-20 22:54:19 -0600

    CDK working

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,115 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
+import { ApplicationProtocol, ListenerAction } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53_targets from 'aws-cdk-lib/aws-route53-targets';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as cert from 'aws-cdk-lib/aws-certificatemanager'
+import { Construct } from 'constructs';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    const updatedProps = {
+      ...props,
+      env: { ...props?.env, region: 'us-east-1', account: '099448516820' },
+    };
+    super(scope, id, updatedProps);
+
+    const vpc = new ec2.Vpc(this, 'PgaGptVpc', {
+      maxAzs: 3,
+    });
+
+    const cluster = new ecs.Cluster(this, 'PgaGptCluster', {
+      vpc: vpc,
+    });
+
+    const appSecret = secretsmanager.Secret.fromSecretNameV2(
+      this,
+      'PgaGptSecret',
+      'pga-gpt-secrets',
+    );
+
+    // IAM inline role - the service principal is required
+    const taskRole = new iam.Role(this, 'fargate-test-task-role', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+
+
+    // Access individual key-value pairs
+    const OKTA_OAUTH2_CLIENT_SECRET = appSecret
+      .secretValueFromJson('OKTA_OAUTH2_CLIENT_SECRET')
+      .unsafeUnwrap();
+    const OKTA_OAUTH2_CLIENT_ID = appSecret
+      .secretValueFromJson('OKTA_OAUTH2_CLIENT_ID')
+      .unsafeUnwrap();
+    const OKTA_OAUTH2_ISSUER = appSecret
+      .secretValueFromJson('OKTA_OAUTH2_ISSUER')
+      .unsafeUnwrap();
+    const SECRET = appSecret.secretValueFromJson('SECRET').unsafeUnwrap();
+    const OPENAI_API_KEY = appSecret
+      .secretValueFromJson('OPENAI_API_KEY')
+      .unsafeUnwrap();
+
+    const repository = ecr.Repository.fromRepositoryArn(
+      this,
+      'PgaGptImage',
+      'arn:aws:ecr:us-east-1:099448516820:repository/pga-gpt',
+    );
+
+    const albLoadBalancer =
+      new ecs_patterns.ApplicationLoadBalancedFargateService(
+        this,
+        'MyFargateService',
+        {
+          cluster: cluster, // Required
+          cpu: 512, // Default is 256
+          desiredCount: 2, // Default is 1
+          taskImageOptions: {
+            image: ecs.ContainerImage.fromEcrRepository(repository, 'latest'),
+            containerPort: 3000,
+            environment: {
+              OKTA_OAUTH2_CLIENT_ID,
+              OKTA_OAUTH2_CLIENT_SECRET,
+              OKTA_OAUTH2_ISSUER,
+              SECRET,
+              OPENAI_API_KEY,
+              NEXTAUTH_URL: 'https://dev.chatgpt.pgahq.com'
+            },
+            taskRole,
+            
+          },
+          memoryLimitMiB: 3072, // Default is 512
+          publicLoadBalancer: true, // Default is false
+        },
+      );
+
+      albLoadBalancer.targetGroup.configureHealthCheck({
+        path: '/api/healthcheck'
+      })
+
+      const httpsCert = cert.Certificate.fromCertificateArn(this, 'HttpsCert', 'arn:aws:acm:us-east-1:099448516820:certificate/a15db74b-b8b5-4965-84d2-8db10723905c');
+      const wwwCert = cert.Certificate.fromCertificateArn(this, 'WwwCert', 'arn:aws:acm:us-east-1:099448516820:certificate/6d13bc64-50cc-47c7-90f1-4a9a813fd6c1');
+
+      albLoadBalancer.loadBalancer.addListener('SSL', {
+        port: 443,
+        certificates: [httpsCert, wwwCert],
+        protocol: ApplicationProtocol.HTTPS,
+        defaultTargetGroups:  [albLoadBalancer.targetGroup]
+      })
+
+    const hostedZone = route53.HostedZone.fromLookup(this, 'PgaHostedDns', {
+      domainName: 'dev.chatgpt.pgahq.com', // Replace with your own domain name
+    });
+
+    new route53.ARecord(this, 'PgaGptAliasRecord', {
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(
+        new route53_targets.LoadBalancerTarget(albLoadBalancer.loadBalancer),
+      ),
+    });
+  }
+}
