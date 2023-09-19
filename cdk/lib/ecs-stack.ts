ecb3f018ceab6c66c0a900a4fdcca0dc3d7c5618
Aman Saran <aman06@gmail.com>
2023-09-19 14:19:55 -0600

    Revert "try adding ecr deploy"
    This reverts commit 93c1febd6e8e28cd6fb343591d212a588fc24212.

--- cdk/lib/ecs-stack.ts
+++ cdk/lib/ecs-stack.ts
@@ -11,14 +11,13 @@ import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import * as cert from 'aws-cdk-lib/aws-certificatemanager'
 import { Construct } from 'constructs';
 import { DockerImageAsset, } from 'aws-cdk-lib/aws-ecr-assets';
-import { DockerImageName, ECRDeployment } from 'cdk-ecr-deployment'
 import path = require('path');
 
 export class EcsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     const updatedProps = {
       ...props,
-      env: { ...props?.env, region: 'us-east-1' },
+      env: { ...props?.env, region: 'us-east-1', account: process.env.CDK_DEFAULT_ACCOUNT },
     };
     super(scope, id, updatedProps);
 
@@ -29,11 +28,6 @@ export class EcsStack extends Stack {
       invalidation: {
         buildArgs: false,
       },
-    })
-
-    new ECRDeployment(this, 'DeployPgaAiDockerImage', {
-      src: new DockerImageName(dockerImage.imageUri),
-      dest: new DockerImageName(`117031054835.dkr.ecr.us-east-1.amazonaws.com/cdk-hnb659fds-container-assets-117031054835-us-east-1:latest`)
     })
 
     const vpc = new ec2.Vpc(this, 'PgaGptVpc', {
@@ -106,7 +100,7 @@ export class EcsStack extends Stack {
       'PgaGptImage',
       dockerImage.repository.repositoryArn,
     );
-
+    account: '099448516820'
 
     const albLoadBalancer =
       new ecs_patterns.ApplicationLoadBalancedFargateService(
